6166eefec2488c58d7e41724ffed20f382c05de2
P S Arvind Aravind <aravind@yahoo.com>
2022-06-05 17:11:41 -0400

    Week 1 Assignment corrected

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,33 +6,39 @@ class DishDetail extends Component{
         super(props);
     }
 
-    formatedDate(date) {
-        var dt = new Date(date);
-        let monthNames =["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-
-        var day = dt.getDate() + 1
-        var formatted =
-            monthNames[dt.getMonth()] + " " +
-            (day < 10 ? "0" : "") + day + ", "  +
-            dt.getFullYear();
-        return formatted;
+    renderDish(dish) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                        <CardTitle><h4>{dish.name}</h4></CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>                    
+                </Card>
+            </div>
+        );
     }
 
     renderComments(comments) {
         if (comments != null) {
-            return <div> 
-            {
-                comments.map((comment) => {
-                    return (
-                        <div key={comment.id}>
-                            <li>{comment.comment}</li>
-                            <li>-- {comment.author}, {this.formatedDate(comment.date)}</li>
-                            <br/>
-                        </div>
-                    )
-                })   
-            }
-            </div>
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {
+                            comments.map(comment => {
+                                return (
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </li>
+                                );
+                            })   
+                        }
+                    </ul>
+                </div>
+            );
         }
         else {
             return <div></div>
@@ -40,33 +46,15 @@ class DishDetail extends Component{
     }
 
     render() {
-        const dish = this.props.dish;
-        if (dish != null) {
-            return <>
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name}/>
-                        <CardBody>
-                            <CardTitle><h4>{dish.name}</h4></CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>                    
-                    </Card>
+        if (this.props.dish != null) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderDish(this.props.dish)}
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
                 </div>
-                <div className="col-12 col-md-5 m-1">
-                    <Card className="border-0">
-                        <CardBody>
-                            <CardTitle>
-                                <h4>Comments</h4>
-                            </CardTitle>
-                            <div>
-                                <ul className="list-unstyled">
-                                    {this.renderComments(dish.comments)}
-                                </ul>
-                            </div>
-                        </CardBody>                                            
-                    </Card>
-                </div>
-            </>
+            );
         }
         else {
             return <div></div>
